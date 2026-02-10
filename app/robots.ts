07ada5151b3ccd9193092486d01dda7babf7fa17
Harkor421 <66706751+Harkor421@users.noreply.github.com>
2026-02-09 19:42:24 -0500

    ds

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://tecnoprints.com/sitemap.xml',
+  }
+}
